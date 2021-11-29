1ec12c08beb95dca37e98940bfd67d113c1fd4b4
0x-2FA <2fa_gh@protonmail.com>
2021-11-29 18:47:23 +0200

    Removed Jade and added app.listen at port 3000

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -36,6 +36,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(3000, () =>{
+  console.log('App is running on: http://localhost:3000/');
 });
 
 module.exports = app;

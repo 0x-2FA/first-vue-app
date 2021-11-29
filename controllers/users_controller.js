33ff8c932aee84b8ba77fcbf5bd6c5e668214d55
0x-2FA <2fa_gh@protonmail.com>
2021-11-29 20:28:52 +0200

    Added pg module to start working on models.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,6 +2,6 @@ module.exports.controller = app => {
 
     // get users page
     app.get('/users', (req, res) => {
-        res.render('users', {title: 'Users'});
+        res.render('users', {title: 'Users', description: 'In this page you will find everything about the users of the site.'});
     });
 };
